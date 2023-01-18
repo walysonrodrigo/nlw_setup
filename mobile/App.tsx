2425eb6a929ed69eab519a21ca90e10af911956e
walysonrodrigo <walysonrodrigo@gmail.com>
2023-01-18 19:46:41 -0300

    first class

---
+++ mobile/App.tsx
@@ -0,0 +1,46 @@
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontLoaded) {
+    return(
+      <Loading />
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Oi vidinha</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: '#ffff',
+    fontFamily: 'Inter_800ExtraBold'
+  }
+});
---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors';
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title : {
+                startsWith : 'B'
+            }
+        }
+    })
+
+    return habits
+})
+
+app.listen({
+    port:3333
+}).then(() => {
+    console.log("SERVER RUNNING!");
+})
